420afff234c93791c284a788209c03705f8d4c8e
NTSALA KANANELO <ntsalakananelo89@gmail.com>
2025-02-25 11:57:02 +0200

    Create pat1-subtask3binarytodecimal.cpp

---
+++ pat1-subtask3binarytodecimal.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <bitset>
+#include <cmath>
+using namespace std;
+
+//The Function to convert decimal to binary
+string decimalToBinary(string binary){
+  int decimal = 0;
